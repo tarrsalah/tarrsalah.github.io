6fca62064f871ac15175f1ac4639ce94a05db8ae
Salah Eddine Taouririt <tarrsalah@gmail.com>
2026-02-09 21:39:29 +0100

    misc

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,10 +1,10 @@
-module.exports = {
+export default {
 	title: "tarrsalah",
 	url: "https://tarrsalah.com/",
 	language: "en",
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
-	  name: "Salah Eddine Taouririt",
+		name: "Salah Eddine Taouririt",
 		email: "tarrsalah@gmail.com",
 		url: "https://tarrsalah.com/about-me/"
 	}
